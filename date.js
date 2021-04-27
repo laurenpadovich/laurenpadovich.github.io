ae4130f14dcc150a83abd366363980654f0bb329
laurenpadovich <pad17003@byui.edu>
2021-04-26 23:44:28 -0600

    date.js

---
+++ date.js
@@ -0,0 +1,21 @@
+let months = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12"
+];
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthNum = months[d.getMonth()];
+let year = d.getFullYear();
+let fulldate = monthNum + "/" + d.getDate() + "/" + year + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds;
+
+document.getElementById("currentdate").textContent = fulldate;
