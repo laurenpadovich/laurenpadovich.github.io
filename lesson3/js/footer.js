8d7e2e1ade58995622dd4a2b9f7d60276f4682ba
laurenpadovich <pad17003@byui.edu>
2021-05-08 21:03:15 -0600

    site plan

---
+++ lesson3/js/footer.js
@@ -0,0 +1,4 @@
+let date = new Date();
+document.getElementById("currentYear").textContent = date.getFullYear();
+let lastUpdated = document.lastModified;
+document.getElementById("lastUpdated").textContent = lastUpdated;
